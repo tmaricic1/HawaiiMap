5ca07b392bb6529aed85921ef160999ec01d0b43
Tony <tmaricic1@gmail.com>
2024-04-13 19:45:28 -0400

    Added FastAPI and basic Hawaii visualization to React page

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Hawaii Heat Map Project
+
+## Setup
+
+1. Run `python -m venv venv` to create a virtual environment
+2. `pip install -r requirements.txt` to install the dependencies
+
+## Generate the map
+
+1. `uvicorn main:app --reload` to run the fastAPI server
+2. `cd my-app` and then ``npm start` to run the react page
+
+If you do not have npm installed, see the below link (for windows):
+https://www.freecodecamp.org/news/how-to-install-node-js-and-npm-on-windows-2/
+
+If you experience the following error (on MacOS):
+pyspark.errors.exceptions.base.PySparkRuntimeError: [JAVA_GATEWAY_EXITED] Java gateway process exited before sending its port number.
+Run:
+brew install java
+Then link it with the following:
+sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk \
+ /Library/Java/JavaVirtualMachines/openjdk.jdk
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [map, setMap] = useState("");
+
+  useEffect(() => {
+    async function fetchMap() {
+      const response = await fetch("http://localhost:8000/generateMap");
+      const mapObject = await response.json();
+      setMap(`data:image/png;base64,${mapObject.img}`);
+    }
+
+    fetchMap();
+  }, []);
+
+  return <div style={{ textAlign: "center" }}>{map && <img src={map} />}</div>;
 }
 
 export default App;
